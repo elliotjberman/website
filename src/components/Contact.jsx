8f3b5f26abb1b7b12d5257ea9262f73b30be8922
Elliot Berman <elliotjberman@gmail.com>
2017-02-12 16:50:20 +0100

    nice swiss style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,18 +8,90 @@ class Contact extends React.Component {
 	componentDidMount = () => {
 		this.props.setGrayscale(true);
 		document.getElementById('gray-box').className = 'full';
+		document.getElementById('name').style.display = 'none';
+
+		function adjustCircle() {
+			let min = Math.ceil(1);
+			let max = Math.floor(5);
+			let number = Math.floor(Math.random() * (max - min)) + min;
+			let circle = document.getElementById('circle-boy-' + number);
+
+			let randomRadius = Math.random() * 50;
+
+			circle.style.borderRadius = randomRadius + "%";
+		}
+
+		window.setTimeout(function() {
+
+			window.setInterval(adjustCircle, 7000);
+
+		}, 10000);
 	}
 
   render = () => {
 		var path = this.props.location.pathname;
     return (
 	      <div id="contact">
-					<h2>
-						978.729.3655
-						<br/>
-						<a href="mailto:varsitystarmusic@gmail.com">varsitystarmusic@gmail.com</a>
-					</h2>
-					<h1>Contact</h1>
+
+					<div id="intro" className="whole grid">
+						<h1>Contact</h1>
+					</div>
+
+					<div className="grid fifth">
+
+						<div className="circle-container">
+							<div id="circle-boy-0"></div>
+						</div>
+
+
+
+
+					</div>
+					<div className="grid fifth">
+
+						<div className="circle-container">
+							<div id="circle-boy-1"></div>
+						</div>
+
+
+						<h2>Email</h2>
+
+					</div>
+					<div className="grid fifth">
+
+						<div className="circle-container">
+							<div id="circle-boy-2"></div>
+						</div>
+
+
+						<h2>Call</h2>
+
+					</div>
+					<div className="grid fifth">
+
+						<div className="circle-container">
+							<div id="circle-boy-3"></div>
+						</div>
+
+
+						<h2>Listen</h2>
+
+					</div>
+					<div className="grid fifth">
+
+						<div className="circle-container">
+							<div id="circle-boy-4"></div>
+						</div>
+
+
+						<h2>Elliot <span className="contrast">Berman</span></h2>
+						<p>
+							<span className="contrast">is a Brooklyn-based musician currently living in Berlin. </span>
+							Hit him up if you're want music for a live show, a movie, a video game, or anything else you can think of.
+						</p>
+
+					</div>
+
 	      </div>
     );
   }
