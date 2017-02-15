0b4bc3f2fbb54210c63deb13ad87e1f72c7d7bcb
Elliot Berman <elliotjberman@gmail.com>
2017-02-15 16:17:44 +0100

    index?

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+const host = '0.0.0.0';
+const port = '3000';
+
+const app = express();
+app.use(express.static(__dirname+ "/dist"))
+
+app.listen(port, host, (err) => {
+	if(err) console.log(err);
+	console.log(`listening on port ${port}`);
+});
